08d215712565b4cfd3c30c4bbb60f796dc84e632
Florian <58337160+OmengU@users.noreply.github.com>
2022-03-11 10:05:29 +0100

    started defining variables in Classes

--- src/main/java/com/example/monopoly/Dice.java
+++ src/main/java/com/example/monopoly/Dice.java
@@ -1,4 +1,12 @@
 package com.example.monopoly;
 
 public class Dice{
+    private int highestPossible;
+    Dice (int h){
+        highestPossible = h;
+    }
+    public int rollDice(){
+        int rolledNumber = (int)(Math.random()*(highestPossible +1));
+        return rolledNumber;
+    }
 }
--- src/main/java/com/example/monopoly/Player.java
+++ src/main/java/com/example/monopoly/Player.java
@@ -1,4 +1,10 @@
 package com.example.monopoly;
 
 public class Player {
+    private String name;
+    private int balance;
+
+    Player (String name){
+        name = name;
+    }
 }
